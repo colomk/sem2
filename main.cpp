0c06f652d403c946ad626d3c05b0f6ad0a1d0f71
Ярослав Соломенников ТКИ - 141 <yaroslav.solomennikov@yandex.ru>
2026-09-25 19:29:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
  * @param report - строка информации
  * @return считанное значение
  */
-double getValue(std::string report = "");
+double getValue(const std::string report = "");
 
 /**
  * @brief Точка входа в программу
